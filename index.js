c6c63a0c6b6a80dd19d6bbf38297b94bb338c01a
Serena Hasler <srhasler@yahoo.com>
2020-04-05 20:22:17 -0500

    Add methods to prototype to check drinking age

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function Person(fname, lname, age, weight, hobbies) {
   this.weight = weight;
   this.hobbies = hobbies;
 }
+Person.prototype.checkAge = function() {
+  if (this.age > 20) {
+    return `${this.fname} is old enough to drink alcohol.`;
+  }
+  return `${this.fname} is NOT old enough to drink alcohol.`;
+};
+
+const jill = new Person("Jill", "Stevens", 12, 100);
+const karla = new Person("Karla", "Bishop", 21, 125);
+console.log(jill.checkAge());
+console.log(karla.checkAge());
