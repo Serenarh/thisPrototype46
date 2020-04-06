46bcc4b8b58dd49b81775a8d19e7803b78d47467
Serena Hasler <srhasler@yahoo.com>
2020-04-05 20:26:06 -0500

    Add array of strings to represent hobbies and add prototype to generate list

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function Person(fname, lname, age, weight, hobbies) {
   this.age = age;
   this.weight = weight;
   this.hobbies = hobbies;
+
+  if (this.fname === "Jill") {
+    this.hobbies = ["knitting", " running", " and baseball."];
+  } else if (this.fname === "Karla") {
+    this.hobbies = ["judo", " baking", " and reading."];
+  } else {
+    this.hobbies = ["welding", " drawing", " and painting."];
+  }
 }
 Person.prototype.checkAge = function() {
   if (this.age > 20) {
@@ -12,7 +20,15 @@ Person.prototype.checkAge = function() {
   return `${this.fname} is NOT old enough to drink alcohol.`;
 };
 
+Person.prototype.getInfo = function() {
+  return `${this.fname} ${this.lname} is ${this.age} years old and weighs ${this.weight} pounds. Her hobbies are ${this.hobbies}`;
+};
+
 const jill = new Person("Jill", "Stevens", 12, 100);
 const karla = new Person("Karla", "Bishop", 21, 125);
+
+console.log(jill.getInfo());
+console.log(karla.getInfo());
+
 console.log(jill.checkAge());
 console.log(karla.checkAge());
